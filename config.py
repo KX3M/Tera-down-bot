f6948f076d19c57ea25a8a26b681a7c7f7d0e83f
KX3M <163962831+KX3M@users.noreply.github.com>
2025-04-21 04:54:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "2e7e4739d495b5fce0531b9dc8342c40762a3fa9")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/ChipsTutorial/7") # shareus ka tut_vid he 
